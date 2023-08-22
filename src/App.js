06c24f95457243105e42bdeabde013e7b677aa5d
monahvi <nahvi.moghaddam@gmail.com>
2023-08-22 20:30:54 +0100

    PropType

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Button from "./Button";
+
 function App() {
-    return <div>App</div>
+    return <div>
+        <div>
+            <Button >Click me!</Button>
+        </div>
+        <div>
+            <Button>Buy now!</Button>
+        </div>
+        <div>
+            <Button>See deal!</Button>
+        </div>
+        <div>
+            <Button>Hide ads!</Button>
+        </div>
+        <div>
+            <Button>Somthing!</Button>
+        </div>
+    </div>
 }
 
 export default App;
