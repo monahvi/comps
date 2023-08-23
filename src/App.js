8b6601a4146ed334899d1c560d23afb42ae13d4d
monahvi <nahvi.moghaddam@gmail.com>
2023-08-23 21:03:32 +0100

    classnames

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import Button from "./Button";
+import Button from './Button';
 
 function App() {
-    return <div>
-        <div>
-            <Button >Click me!</Button>
-        </div>
-        <div>
-            <Button>Buy now!</Button>
-        </div>
-        <div>
-            <Button>See deal!</Button>
-        </div>
-        <div>
-            <Button>Hide ads!</Button>
-        </div>
-        <div>
-            <Button>Somthing!</Button>
-        </div>
+  return (
+    <div>
+      <div>
+        <Button success rounded outline>
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>See Deal!</Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button primary rounded>
+          Something!
+        </Button>
+      </div>
     </div>
+  );
 }
 
 export default App;
