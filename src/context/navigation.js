85511e074278cdde8d538d1f726eada2ac95bb88
monahvi <nahvi.moghaddam@gmail.com>
2023-08-29 12:23:34 +0100

    navigation

--- src/context/navigation.js
+++ src/context/navigation.js
@@ -1,12 +1,33 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 
 const NavigationContext = createContext();
 
 function NavigationProvider({children}) {
-    return <NavigationContext.Provider value={{}}>
+    const [ currentPath, SetcurrentPath] = useState(window.location.pathname);
+
+    useEffect(()=> {
+        const handler = ()=>{
+            SetcurrentPath(window.location.pathname);
+        };
+        window.addEventListener('popstate',handler);
+
+        return () => {
+            window.removeEventListener('popstate',handler);
+        };
+    }, []);
+
+    const navigate = (to) => {
+        window.history.pushState({}, '', to);
+        SetcurrentPath(to);
+    };
+
+
+    return (  
+    <NavigationContext.Provider value={{ currentPath, navigate}}>
         {children}
     </NavigationContext.Provider>
+    );
 }
 
 export {NavigationProvider};
